704e555576354ccac96e94c8b44449a0141c8d03
MarkMutt <lleasy8@gmail.com>
2022-12-10 18:53:36 -0500

    add tailwinds css

--- simple-form/app/routes/index.tsx
+++ simple-form/app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+    <h1 className="text-4xl font-bold underline text-slate-600">Hello world!</h1>
       <ul>
         <li>
           <a

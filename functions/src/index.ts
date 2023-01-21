e4c18c326d2268ce48f7de605061db8e2d6487e5
MarkMutt <lleasy8@gmail.com>
2023-01-20 19:23:01 -0500

    set tsconfigRootDir

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
-import * as functions from "firebase-functions";
+/* eslint-disable object-curly-spacing */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { functions } from "./firebase";
+import * as express from "express";
+import type { Request, Response, NextFunction } from "express";
+const app = express();
+app.use(express.json());
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  return res.status(200).json({ status: "success" });
+});
+
+app.get("/error", (req: Request, res: Response, next: NextFunction) => {
+  throw new Error("this is error");
+});
+
+
+
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const text = err.message;
+  res.status(500).json({ error: "from middleware", errorMessage: text });
+});
+
+exports.app = functions.https.onRequest(app);
